964e62822363f27ee5e7c6d2d7db2d6574f13d74
twinbladeRoG <sohan.dutta11@gmail.com>
2020-02-25 11:07:27 +0530

    update: Async/Await implemented in controllers

--- src/controllers/ParticipantController.js
+++ src/controllers/ParticipantController.js
@@ -7,14 +7,9 @@ const ParticipantController = {
    * @method GET
    * @description Get all Participants
    */
-	getAll: (req, res) => {
-		Participant.find()
-			.then(participants => {
-				res.json(participants);
-			})
-			.catch(error =>
-				res.status(500).json({ message: 'DB Error', error})
-			);
+	getAll: async (req, res) => {
+		const participants = await Participant.find();
+		return res.json(participants);
 	},
 
 	/**
@@ -22,16 +17,15 @@ const ParticipantController = {
    * @method GET
    * @description Get a Participant by Id
    */
-	getById: (req, res) => {
+	getById: async (req, res) => {
 		const { id } = req.params;
 
-		Participant.find({ _id: id})
-			.then(participants => {
-				res.json(participants);
-			})
-			.catch(error =>
-				res.status(500).json({ message: 'DB Error', error})
-			);
+		try {
+			const participants = await Participant.find({ _id: id});
+			return res.json(participants);
+		} catch (e) {
+			return res.status(500).json({ message: 'DB Error', e});
+		}
 	},
 
 	/**
@@ -39,7 +33,7 @@ const ParticipantController = {
    * @method POST
    * @description Filter out Participants based on search parameters
    */
-	filter: (req, res) => {
+	filter: async (req, res) => {
 		const filter = {};
 		if ('first_name' in req.body) filter.first_name = req.body.first_name;
 		if ('last_name' in req.body) filter.last_name = req.body.last_name;
@@ -47,16 +41,14 @@ const ParticipantController = {
 		if ('email' in req.body) filter.email = req.body.email;
 		if ('institute' in req.body) filter.institute = req.body.institute;
 
-		Participant.find(filter)
-			.then(participants => {
-				log.info(`${participants.length} found!`);
-				res.json(participants);
-			})
-			.catch(error =>
-				res.status(500).json({ message: 'DB Error', error})
-			);
-	},
-
+		try {
+			const participants = await Participant.find(filter);
+			log.info(`${participants.length} found!`);
+			return res.json(participants);
+		} catch (e) {
+			res.status(500).json({ message: 'DB Error', e});
+		}
+	}
 };
 
 module.exports = ParticipantController;
