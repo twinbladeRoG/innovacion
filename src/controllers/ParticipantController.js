7ee5f22074ff0357dfa0e875ef03590294c894cd
twinbladeRoG <sohan.dutta11@gmail.com>
2020-02-25 14:46:53 +0530

    update: CRUD completed for participants

--- src/controllers/ParticipantController.js
+++ src/controllers/ParticipantController.js
@@ -31,6 +31,76 @@ const ParticipantController = {
 	/**
    * @url /api/participants
    * @method POST
+   * @description
+   */
+	create: async (req, res) => {
+		const { first_name, last_name, contact, email, institute } = req.body;
+
+		try {
+			const exists = await Participant.exists({ email });
+
+			if (exists) {
+				return res.status(400).json({ message: 'Participant with same email already exists!'});
+			} else {
+				const participant = await Participant({
+					first_name, last_name, contact, email, institute
+				});
+				participant.save();
+				return res.json(participant);
+			}
+		} catch (e) {
+			return res.status(500).json({ message: 'DB Error', e});
+		}
+	},
+
+	/**
+   * @url /api/participants
+   * @method PUT
+   * @description
+   */
+	update: async (req, res) => {
+		const updates = {};
+		if ('first_name' in req.body) updates.first_name = req.body.first_name;
+		if ('last_name' in req.body) updates.last_name = req.body.last_name;
+		if ('contact' in req.body) updates.contact = req.body.contact;
+		if ('email' in req.body) updates.email = req.body.email;
+		if ('institute' in req.body) updates.institute = req.body.institute;
+
+		const id = req.body._id;
+
+		try {
+			const count = await Participant.updateOne({ _id: id }, updates);
+			return res.json(count);
+		} catch (e) {
+			return res.status(400).json({ message: 'DB Error', e});
+		}
+	},
+
+	/**
+   * @url /api/participants/filter
+   * @method POST
+   * @description Filter out Participants based on search parameters
+   */
+	remove: async (req, res) => {
+		const { id } = req.params;
+
+		try {
+			const exists = await Participant.exists({ _id: id });
+
+			if (exists) {
+				const deleted = await Participant.deleteOne({ _id: id });
+				return res.json(deleted);
+			} else {
+				return res.status(400).json({ message: 'No such participant exists' });
+			}
+		} catch (e) {
+			return res.status(400).json({ message: 'DB Error', e});
+		}
+	},
+
+	/**
+   * @url /api/participants/filter
+   * @method POST
    * @description Filter out Participants based on search parameters
    */
 	filter: async (req, res) => {
@@ -48,7 +118,7 @@ const ParticipantController = {
 		} catch (e) {
 			res.status(500).json({ message: 'DB Error', e});
 		}
-	}
+	},
 };
 
 module.exports = ParticipantController;
--- src/routes/api/participant.js
+++ src/routes/api/participant.js
@@ -5,6 +5,9 @@ const { ParticipantController } = require('../../controllers');
 
 router.get('/', ParticipantController.getAll);
 router.get('/:id', ParticipantController.getById);
+router.post('/', ParticipantController.create);
+router.put('/', ParticipantController.update);
+router.delete('/:id', ParticipantController.remove);
 router.post('/filter', ParticipantController.filter);
 
 module.exports = router;
